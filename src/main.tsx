461fb2fb14e824187038dfd842d639c63959bc72
pleasematheus <matheus4k@gmail.com>
2024-10-20 21:29:43 -0400

    Add error, login, and registration routes; update TypeScript and Vite configurations

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
 import App from "./routes/App.tsx"
+import Erro from "./routes/Erro.tsx"
+
 import "./index.css"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    // errorElement: <Erro/>
+    errorElement: <Erro/>
   },
 ])
 
---
+++ src/routes/Erro.tsx
@@ -0,0 +1,3 @@
+export default function Erro() {
+  return <h1>Erro 404</h1>;
+}
